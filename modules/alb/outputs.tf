1af3b07d65f867845c15f2828998db8863a6d1ea
Sakib Khan <khsakib2005@gmail.com>
2026-06-30 20:07:11 +0530

    Solved Duplicate output issue

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -8,20 +8,6 @@ output "alb_arn" {
   value       = aws_lb.main.arn
 }
 
-output "target_group_arn" {
-  description = "Target group ARN"
-  value       = aws_lb_target_group.web.arn
-}
-output "alb_dns_name" {
-  description = "ALB DNS name"
-  value       = aws_lb.main.dns_name
-}
-
-output "alb_arn" {
-  description = "ALB ARN"
-  value       = aws_lb.main.arn
-}
-
 output "alb_arn_suffix" {
   description = "ALB ARN suffix for CloudWatch"
   value       = aws_lb.main.arn_suffix
