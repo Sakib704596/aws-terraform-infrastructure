ce7e2db18e0b753ab917e032e055abce98e460d8
Sakib Khan <khsakib2005@gmail.com>
2026-06-30 11:19:52 +0530

    Fix Terraform version in CI/CD pipeline

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "s3" {
-    bucket       = "my-terraform-state-bucket-b4f62d9e"
-    key          = "dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "YOUR-BUCKET-NAME"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    # Removed use_lockfile!
+    # Removed dynamodb_table!
   }
 
   required_providers {
